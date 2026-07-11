1098420270d8ed9d874e4146ff410818a0ca8527
hikmal_a.s <93532269+MituBaby@users.noreply.github.com>
2026-07-11 12:27:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ let state = {
 
 // --- PATH ASSET TWIBBON ---
 // Ganti string di bawah ini menjadi path file twibbon asli milik Anda, contoh: 'img/twibbon-template.png'
-const TWIBBON_SRC = 'template.png'; 
+const TWIBBON_SRC = 'twibbon.png'; 
 
 // Load Awal: Inisialisasi Template Twibbon
 twibbonImg.crossOrigin = 'anonymous';
